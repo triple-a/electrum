23756fd5d752acfdc893cdc324820686a5087feb
Steve Guo <steve.guo@triple-a.io>
2023-12-17 17:15:28 +0800

    Format code

--- doc.go
+++ doc.go
@@ -11,40 +11,37 @@ automatically closed by the client instance when the subscription is terminated.
 
 The client supports TCP and TSL connections.
 
-Creating a Client
-
+# Creating a Client
 
 First start a new client instance
 
-  client, _ := electrum.New(&electrum.Options{
-    Address:   "node.xbt.eu:50002",
-    KeepAlive: true,
-  })
+	client, _ := electrum.New(&electrum.Options{
+	  Address:   "node.xbt.eu:50002",
+	  KeepAlive: true,
+	})
 
-
-Synchronous Operations
+# Synchronous Operations
 
 Execute operations as regular methods
 
-  version, _ := client.ServerVersion()
+	version, _ := client.ServerVersion()
 
-Subscriptions
+# Subscriptions
 
-Get notifications using regular channels and context
+# Get notifications using regular channels and context
 
+	ctx, cancel := context.WithTimeout(context.Background(), 30 * time.Second)
+	defer cancel()
+	headers, _ := client.NotifyBlockHeaders(ctx)
+	  for header := range headers {
+	  // Use header
+	}
 
-  ctx, cancel := context.WithTimeout(context.Background(), 30 * time.Second)
-  defer cancel()
-  headers, _ := client.NotifyBlockHeaders(ctx)
-    for header := range headers {
-    // Use header
-  }
-
-Terminating a Client
+# Terminating a Client
 
 When done with the client instance free-up resources and terminate network communications
 
-  client.Close();
+	client.Close();
 
 Protocol specification is available at:
 http://docs.electrum.org/en/latest/protocol.html
